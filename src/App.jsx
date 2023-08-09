31c196da3ca6beb33bf67ce839df7c896060d673
Yannik195 <yannisim@gmail.com>
2023-08-09 11:58:04 +0200

    remove node_modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const [data, setData] = useState();
 
   useEffect(() => {
