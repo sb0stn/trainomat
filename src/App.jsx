d3fd66ff12bc39b560c834fb0f3d97a51ab4c62c
Yannik195 <yannisim@gmail.com>
2023-08-14 14:32:39 +0200

    search

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState, useEffect, Fragment } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, Form } from "react-router-dom";
 import "./App.css";
 import ListItem from "./components/ListItem";
 
@@ -12,6 +11,13 @@ function App() {
 
   return (
     <main>
+      <div>
+        <Form method="get" action="/">
+          <input aria-label="search products" type="text" name="q" />
+          <button type="submit">Search</button>
+        </Form>
+      </div>
+
       <table>
         <thead style={{ textAlign: "left" }}>
           <tr>
@@ -31,15 +37,19 @@ function App() {
   );
 }
 
-export async function loader() {
+export async function loader({ request }) {
   const ZOTERO_API_BASE_URL = "https://api.zotero.org";
   const ZOTERO_GROUP_ID = "2580211";
+
+  console.log(request);
+  let url = new URL(request.url);
+  let searchTerm = url.searchParams.get("q");
 
   const items = await (
     await fetch(
       //`${ZOTERO_API_BASE_URL}/groups/${ZOTERO_GROUP_ID}/items?start=194&limit=100` //limit
-      //`${ZOTERO_API_BASE_URL}/groups/${ZOTERO_GROUP_ID}/items?q=ios` //search
-      `${ZOTERO_API_BASE_URL}/groups/${ZOTERO_GROUP_ID}/items?tag=(P) Xcode` //tag
+      `${ZOTERO_API_BASE_URL}/groups/${ZOTERO_GROUP_ID}/items?q=${searchTerm}&tag=(P) Xcode` //search
+      //`${ZOTERO_API_BASE_URL}/groups/${ZOTERO_GROUP_ID}/items?tag=(P) Xcode` //tag
     )
   ).json();
 
