37f2b9de549aaa0bfe41b53d2ec13c486b18e3ac
Yannik195 <yannisim@gmail.com>
2023-08-20 14:01:20 +0200

    add react query to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App, { loader as itemsLoader } from "./App.jsx";
 import ItemPage from "./pages/ItemPage.jsx";
 import SearchResults from "./components/SearchResults.jsx";
 import "./index.css";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -27,6 +31,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools position="bottom-right" />
+    </QueryClientProvider>
   </>
 );
