e1ddc4bb8848194b47bed5fc024142c40b0b3017
Yannik195 <yannisim@gmail.com>
2023-08-22 14:40:00 +0200

    react-dom/client vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react", "react-dom"]
+      external: ["react", "react-dom", "react-dom/client"]
     }
   }
 })
