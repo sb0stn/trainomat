ef0711fd18ffa91b646e802750dde807e24a1dd6
Yannik195 <yannisim@gmail.com>
2023-08-22 13:36:36 +0200

    move components folder to routes folder

--- src/routes/Root.jsx
+++ src/routes/Root.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import SearchBar from "../components/SearchBar.jsx";
+import SearchBar from "./components/SearchBar.jsx";
 import "./Root.css";
 
 function Root() {
