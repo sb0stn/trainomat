6d22cf5cabf3d0a7730d8ac1d8a763183e092144
Yannik195 <yannisim@gmail.com>
2023-08-22 13:46:00 +0200

    vite config import react

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react-dom/client', 'react-dom']
+      external: [react]
     }
   }
 })
