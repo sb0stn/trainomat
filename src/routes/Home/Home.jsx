5285a89321874f71365dfb139633705215064570
Yannik195 <yannisim@gmail.com>
2023-08-25 02:24:49 +0200

    header refactor

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main>
       <SearchBar />
-      <ItemList />;
+      <ItemList />
     </main>
   );
 }
--- src/routes/components/AppBar/AppBar.jsx
+++ src/routes/components/AppBar/AppBar.jsx
@@ -1,25 +1,45 @@
 import logo from "../../../assets/logo.svg";
 import styles from "./AppBar.module.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function AppBar() {
   return (
     <header className={styles.header}>
-      <div className={styles.logo_container}>
-        <img className={styles.logo_svg} src={logo} alt="Logo" />
-        <span className={styles.logo_text}>TRAINOMAT</span>
+      <div className={styles.header_container}>
+        <div className={styles.logo_container}>
+          <img className={styles.logo_svg} src={logo} alt="Logo" />
+          <div className={styles.logo}>
+            <span className={styles.logo_text}>TRAINOMAT</span>
+            <p className={styles.slogan}>
+              Ein Projekt des Kompetenzzentrum für digitale Barrierefreiheit
+            </p>
+          </div>
+        </div>
+        <nav>
+          <ol>
+            <li>
+              <NavLink
+                to={"/"}
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? styles.active : styles.link
+                }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to={"/about"}
+                className={({ isActive, isPending }) =>
+                  isPending ? "pending" : isActive ? styles.active : styles.link
+                }
+              >
+                About
+              </NavLink>
+            </li>
+          </ol>
+        </nav>
       </div>
-      <nav>
-        <ol>
-          <li>
-            <Link to={"/"}>Home</Link>
-          </li>
-          <li>
-            {" "}
-            <Link to={"/about"}>About</Link>
-          </li>
-        </ol>
-      </nav>
     </header>
   );
 }
