7f8bcc840c14e01693e380e78830da9c80128b79
Yannik195 <yannisim@gmail.com>
2023-10-09 16:39:09 +0200

    date and title sort

--- src/routes/Home/components/ItemList/ItemList.jsx
+++ src/routes/Home/components/ItemList/ItemList.jsx
@@ -1,31 +1,36 @@
 import React, { useEffect } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import ListItem from "../../../components/ListItem/ListItem.jsx";
 import styles from "./ItemList.module.css";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { useInView } from "react-intersection-observer";
 import LoadNewPageIndicator from "./components/LoadNewPageIndicator.jsx";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchResults() {
   const { ref, inView } = useInView();
-  const { q, tags } = useLoaderData();
+  const navigate = useNavigate();
+  const { q, tags, sort, direction } = useLoaderData();
   const {
     data: items,
     status,
+    error,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
-  } = useInfiniteQuery(itemsQuery(q, tags));
-
-  if (!items) {
-    return <p>loading</p>;
-  }
+  } = useInfiniteQuery(itemsQuery(q, tags, sort, direction));
 
   useEffect(() => {
     if (inView && hasNextPage) {
       fetchNextPage();
     }
   }, [inView]);
+
+  function handleSort(sortParam) {
+    const reverseDirection = direction === "asc" ? "desc" : "asc";
+    navigate(`?sort=${sortParam}&direction=${reverseDirection}`);
+  }
 
   return status === "loading" ? (
     <p>Loading...</p>
@@ -40,8 +45,22 @@ export default function SearchResults() {
         <table className={styles.table}>
           <thead style={{ textAlign: "left" }}>
             <tr className={styles.row}>
-              <th>Titel</th>
-              <th>Datum</th>
+              <th onClick={() => handleSort("title")}>
+                Titel
+                {direction == "desc" && sort == "title" ? (
+                  <FontAwesomeIcon icon={faChevronUp} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronDown} />
+                )}
+              </th>
+              <th onClick={() => handleSort("dateAdded")}>
+                Datum
+                {direction == "desc" && sort == "dateAdded" ? (
+                  <FontAwesomeIcon icon={faChevronUp} />
+                ) : (
+                  <FontAwesomeIcon icon={faChevronDown} />
+                )}
+              </th>
               {/* <th>Rating</th> */}
               <th>Typ</th>
               {/* <th>Rolle</th> */}
@@ -77,19 +96,20 @@ export default function SearchResults() {
   );
 }
 
-const itemsQuery = (q, tags) => ({
-  queryKey: ["items", q, tags],
+const itemsQuery = (q, tags, sort, direction) => ({
+  queryKey: ["items", q, tags, sort, direction],
   queryFn: async ({ pageParam = 0 }) => {
     const url = new URL("https://api.zotero.org/groups/4624031/items");
-
-    q && url.searchParams.append("q", q);
 
     tags.forEach((tag) => {
       url.searchParams.append("tag", tag);
     });
 
+    q && url.searchParams.append("q", q);
     url.searchParams.append("limit", 100);
     url.searchParams.append("start", pageParam);
+    url.searchParams.append("sort", sort ?? "title");
+    url.searchParams.append("direction", direction ?? "asc");
 
     const response = await fetch(url);
 
@@ -121,11 +141,13 @@ export const loader =
     const url = new URL(request.url);
     const q = url.searchParams.get("q") ?? "";
     const tags = url.searchParams.getAll("tags");
+    const sort = url.searchParams.get("sort");
+    const direction = url.searchParams.get("direction");
 
     const query = itemsQuery(q, tags);
 
     queryClient.getQueryData(query.queryKey) ??
       (await queryClient.fetchInfiniteQuery(query));
 
-    return { q, tags };
+    return { q, tags, sort, direction };
   };
