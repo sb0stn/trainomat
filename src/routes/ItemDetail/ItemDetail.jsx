c4e9a9c4d4b4aa8dc49f9b6afc8d9e27af1ce4ff
Yannik195 <yannisim@gmail.com>
2023-11-11 14:21:09 +0100

    item detail display zotero link and tag color

--- src/routes/ItemDetail/ItemDetail.jsx
+++ src/routes/ItemDetail/ItemDetail.jsx
@@ -1,15 +1,13 @@
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faExternalLinkAlt } from "@fortawesome/free-solid-svg-icons";
 import styles from "./ItemDetail.module.css";
 import Tag from "./components/Tag.jsx";
 import Note from "./components/Note.jsx";
 
 function ItemDetail() {
   let { state } = useLocation();
-  const navigate = useNavigate();
 
-  console.log(state);
   return (
     <main className={styles.main}>
       <h1 style={{ lineHeight: "100%" }}>{state.data.title}</h1>
@@ -23,6 +21,7 @@ function ItemDetail() {
         </a>
       </div>
       <p>{state.data.abstractNote}</p>
+
       <ul className={styles.tagsList}>
         {state.data.tags.map((tag, index) => {
           return <Tag key={index} tag={tag.tag} />;
@@ -47,14 +46,21 @@ function ItemDetail() {
             <td>{state.data.date}</td>
           </tr>
           <tr>
-            <th>Zotero Link</th>
+            <th>Zotero</th>
             <td>
               <a
                 href={state.links.alternate.href}
                 target="_blank"
-                style={{ wordBreak: "break-all" }}
+                rel="noopener noreferrer"
+                style={{ display: "flex", alignItems: "center" }}
               >
-                {state.links.alternate.href}
+                <span style={{ wordBreak: "break-all" }}>In Zotero öffnen</span>
+                <span style={{ marginLeft: "5px" }}>
+                  <FontAwesomeIcon
+                    icon={faExternalLinkAlt}
+                    aria-label="In Zotero öffnen"
+                  />
+                </span>
               </a>
             </td>
           </tr>
