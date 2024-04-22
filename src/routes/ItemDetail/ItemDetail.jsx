d6ac5a8ecff02da46e36863e879aa565ec39fdb4
Sebastian Koch <sebastian@Sebastians-MacBook-Pro.local>
2024-04-22 13:58:57 +0200

    Fix issue accessing item directly or in new tab

--- src/routes/ItemDetail/ItemDetail.jsx
+++ src/routes/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,6 @@
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 import {
   faExternalLinkAlt,
@@ -10,16 +10,37 @@ import styles from "./ItemDetail.module.scss";
 import Tag from "./components/Tag.jsx";
 import Note from "./components/Note.jsx";
 import DocumentInformation from "./components/DocumentInformation";
+import Skeleton from "react-loading-skeleton";
 
 function ItemDetail() {
-  let { state } = useLocation();
-  let navigate = useNavigate();
+  const { state } = useLocation();
+  const [data, setData] = useState(null);
+
 
   useEffect(() => {
-    document.title = `${state.data.title} - TRAINomat`;
+    async function fetchData() {
+      const href = window.location.href;
+      const regex = /\/items\/([A-Z0-9]+)$/;
+      const id = (regex.exec(href))[1];
+
+      
+      const url = new URL(`https://api.zotero.org/groups/4624031/items/${id}`);
+      const response = await fetch(url);
+      const result = await response.json();
+      setData(result);
+    }
+
+   if (state == null){
+    fetchData();
+   } else {
+    setData(state);
+   }
+
+
+    // document.title = `${state.data.title} - TRAINomat`;
   }, []);
 
-  return (
+  return data == null ? <Skeleton count={10}/> : (
     <main className={styles.main}>
       <nav aria-label="Zurück">
         <a className={styles.link} href="/">
@@ -27,19 +48,19 @@ function ItemDetail() {
         <span>Zurück</span>
         </a>
       </nav>
-      <h1 lang={state.data.language} style={{ lineHeight: "1.2" }}>
-        {state.data.title}
+      <h1 lang={data.data.language} style={{ lineHeight: "1.2" }}>
+        {data.data.title}
       </h1>
 
       <section aria-labelledby="ressourceHeading" className={styles.ressource}>
         <a
-          href={state.data.url}
+          href={data.data.url}
           target="_blank"
           className={styles.resouceHeading}
           style={{ wordBreak: "break-all" }}
         >
           <h2 id="ressourceHeading">Ressource <span className="sr-only">in Zotero</span> öffnen</h2>
-          {state.data.url}
+          {data.data.url}
           <FontAwesomeIcon
             role="presentation"
             className={styles.icon}
@@ -50,24 +71,24 @@ function ItemDetail() {
 
       <section aria-labelledby="description-headline">
         <h2 id="description-headline">Beschreibung</h2>
-        <p lang={state.data.language}>{state.data.abstractNote}</p>
+        <p lang={data.data.language}>{data.data.abstractNote}</p>
       </section>
 
       <section aria-labelledby="tags-headline">
         <h2 id="tags-headline">Tags</h2>
         <ul className={styles.tagsList}>
-          {state.data.tags.map((tag, index) => {
+          {data.data.tags.map((tag, index) => {
             return (
-              <li>
-                <Tag key={index} tag={tag.tag} />
+              <li key={index}>
+                <Tag tag={tag.tag} />
               </li>
             );
           })}
         </ul>
       </section>
-      {state.data.extra && <Note extra={state.data.extra} />}
+      {data.data.extra && <Note extra={data.data.extra} />}
 
-      <DocumentInformation state={state} />
+      <DocumentInformation state={data} />
     </main>
   );
 }
